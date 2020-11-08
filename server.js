dd6ccae128521c859280f7847b2286675236ab85
Khavab <2021snomani@tjhsst.edu>
2020-11-08 15:36:58 -0500

    added hbs and main css

--- server.js
+++ server.js
@@ -6,11 +6,15 @@ const app = express();
 app.use(express.json());
 app.use(express.static("express"));
 
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'static')));
+
 // default URL for website
 app.use('/', function(req,res){
-    res.sendFile(path.join(__dirname+'/express/home.html'));
+    res.render("home");
     //__dirname : It will resolve to your project folder.
   });
+
 const server = http.createServer(app);
 const port = 3000;
 server.listen(port);
